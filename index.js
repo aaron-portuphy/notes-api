e16203da82deef27ea1594ab325c0f2e21178bba
aaronportuphy <aaronportuphy@gmail.com>
2024-07-05 10:12:21 +0000

    Corrected index structure

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import { dbconnect } from './config/db.js';
 //Create a variable and assign to express
 const app = express();
 
-//Listen to your port
-app.listen(8000, ()=>{
-    console.log('App is listening on Port 8000')
-})
+
 
 //Calling our database
 dbconnect();
+
+//Apply to middleware
 app.use(express.json())
 
 //Write a Post Request
@@ -18,4 +17,7 @@ app.post('/note', (req, res)=>{
     res.json('This is a post request');
 })
 
-//Atlas DB Password: NotesAPIAssignment
+//Listen to your port
+app.listen(8000, ()=>{
+    console.log('App is listening on Port 8000')
+})
